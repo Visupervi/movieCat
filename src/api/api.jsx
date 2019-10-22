b4edfe04490d75719321692b66a69fb5abc51b74
Visupervi <Visupervi@outlook.com>
2019-10-22 10:21:06 +0800

    "数据请求，轮播图渲染"

---
+++ src/api/api.jsx
@@ -0,0 +1,19 @@
+import Axios from 'axios';
+// import Toast from '../components/Toast'
+import { Modal, Toast } from 'antd-mobile';
+
+export default function  Ajax(url, data={},type="GET") {
+  return new Promise((resolve, reject) => {
+    let promise;
+    if (type.toLocaleLowerCase() === "get") {
+      promise = Axios.get(url, {params: data})
+    } else {
+      promise = Axios.post(url, data)
+    }
+    promise.then(res => {
+      resolve(res.data)
+    }).catch(err => {
+   Modal.alert("提示",err.message,[ { text: '确定', onPress: () => console.log('ok') },])
+    })
+  })
+}
--- src/components/movie/Carousel/index.jsx
+++ src/components/movie/Carousel/index.jsx
@@ -1,22 +1,55 @@
 import React, {Component} from 'react';
 import {Carousel, WingBlank} from 'antd-mobile';
-import './index.less'
+import {getInTheaters} from '../../../api'
+import './index.less';
+
 class Moviecarousel extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: ['1', '2', '3'],
       imgHeight: 176,
+      carouselImg: []
     }
+  }
+
+  componentWillUpdate(nextProps, nextState, nextContext) {
+
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    this.getImage();
+  }
+
+  componentWillMount() {
+
   }
 
   componentDidMount() {
     // simulate img loading
-    setTimeout(() => {
-      this.setState({
-        data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
-      });
-    }, 100);
+    // setTimeout(() => {
+    //   this.setState({
+    //     : ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
+    //   });
+    // }, 100);
+  }
+
+  //获取轮播图数据函数
+  /**
+   * 需要把image的地址暂存，要不然取不到图片
+   * @returns {Promise<void>}
+   */
+  async getImage() {
+    let res = await getInTheaters();
+    console.log(res);
+    let tempArr = [];
+    for (let i = 0; i < 5; i++) {
+      tempArr[i] = res.subjects[i];
+      let _u = tempArr[i].images.medium.substring(8);
+      tempArr[i].images.medium = 'https://images.weserv.nl/?url=' + _u;
+    }
+    this.setState({
+      carouselImg:tempArr
+    })
+
   }
 
   render() {
@@ -28,14 +61,14 @@ class Moviecarousel extends Component {
           beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
           afterChange={index => console.log('slide to', index)}
         >
-          {this.state.data.map(val => (
+          {this.state.carouselImg.map((item,index) => (
             <a
-              key={val}
+              key={index}
               href="http://www.alipay.com"
               style={{display: 'inline-block', width: '100%', height: this.state.imgHeight}}
             >
               <img
-                src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                src={item.images.medium}
                 alt=""
                 style={{width: '100%', verticalAlign: 'top'}}
                 onLoad={() => {
@@ -51,4 +84,5 @@ class Moviecarousel extends Component {
     )
   }
 }
+
 export default Moviecarousel
